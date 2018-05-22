f026f4c4a8e45dfac7fddfee5f26f1325cb801fc
navdin <navin.dinesh@gmail.com>
2018-05-22 16:57:26 +0530

    hello world edited

--- java-coding/src/helloWorld.java
+++ java-coding/src/helloWorld.java
@@ -1,5 +1,23 @@
+import java.util.Arrays;
+
 public class helloWorld {
     public static void main(String[] args){
         System.out.println("Hello Rand");
+
+        String[] strings = new String[] { "foo", "bar" };
+        Integer a=5;
+        changeReference(strings, a);
+        System.out.println(Arrays.toString(strings)+"  "+strings[0] ); // still [foo, bar]
+        changeValue(strings);
+        System.out.println(Arrays.toString(strings)); // [foo, foo]
+
+
+    }
+    public static void changeReference(String[] strings, Integer a) {
+        strings = new String[] { "foo", "foo" };
+        a.getClass();
+    }
+    public static void changeValue(String[] strings) {
+        strings[1] = "foo";
     }
 }
